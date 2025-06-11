b7dd153b9bd75b45269b4d5ce747db6315224d0f
CSAADZIDI <zidisaad.chaima@gmail.com>
2025-06-11 12:06:35 +0200

    agents.py maj

--- agents/agents.py
+++ agents/agents.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from langchain import hub
 from langchain_ollama import ChatOllama
 from langchain.agents import create_react_agent, AgentExecutor
